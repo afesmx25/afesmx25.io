71eede4642a86ff5e23f06330da4591f2b8ec70b
Ricardo Gore <rjhgore@gmail.com>
2025-10-27 12:21:43 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Add `base: '/afesmx25.io/'` so built asset URLs include the repo name
 export default defineConfig({
+  base: '/afesmx25.io/',
   plugins: [react()],
 })
